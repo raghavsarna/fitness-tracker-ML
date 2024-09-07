634717382fdc5219685a4b8d0de7ea05abe81f9e
Raghav <raghavsarna123@gmail.com>
2024-09-07 22:55:49 +0530

    Remove Outliers

--- src/features/remove_outliers.py
+++ src/features/remove_outliers.py
@@ -227,16 +227,49 @@ for col in outlier_columns:
 # Check outliers grouped by label
 # --------------------------------------------------------------
 
+label = "bench"
+for col in outlier_columns:
+    dataset = mark_outliers_iqr(df[df["label"] == label], col)
+    plot_binary_outliers(dataset=dataset, col=col, outlier_col=col + "_outlier", reset_index=True)
+
+for col in outlier_columns:
+    dataset = mark_outliers_chauvenet(df[df["label"] == label], col)
+    plot_binary_outliers(dataset=dataset, col=col, outlier_col=col + "_outlier", reset_index=True)
+
+dataset, outliers, X_scores = mark_outliers_lof(df, outlier_columns)
+for col in outlier_columns:
+    plot_binary_outliers(dataset=dataset, col=col, outlier_col="outlier_lof", reset_index=True)
 
 # --------------------------------------------------------------
 # Choose method and deal with outliers
 # --------------------------------------------------------------
 
+
+
 # Test on single column
 
+col = "gyr_z"
+dataset = mark_outliers_chauvenet(df, col=col)
+dataset[dataset["gyr_z_outlier"]]
+dataset.loc[dataset["gyr_z_outlier"], "gyr_z"] = np.nan
 
 # Create a loop
+outliers_removed_df = df.copy()
+for col in outlier_columns:
+    for label in df['label'].unique():
+        dataset = mark_outliers_chauvenet(df[df["label"] == label], col)
+        
+        dataset.loc[dataset[col + "_outlier"], col] = np.nan
+        
+        outliers_removed_df.loc[(outliers_removed_df['label'] == label), col] = dataset[col]
+        
+        n_outliers = len(dataset) - len(dataset[col].dropna())
+        print(f"Removed {n_outliers} from {col} in label {label}")
+
+outliers_removed_df.info()
 
 # --------------------------------------------------------------
 # Export new dataframe
 # --------------------------------------------------------------
+
+outliers_removed_df.to_pickle('../../data/interim/02_outliers_removed_chauvnets.pkl')
